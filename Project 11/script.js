abee14163909dae2b6da727fb37c5466b162cc31
devanshkhatri12 <dkkhatri941@gmail.com>
2025-07-10 20:55:49 +0530

    CSS + JS clock

---
+++ Project 11/script.js
@@ -0,0 +1,24 @@
+
+const secondHand = document.querySelector('.sec-hand')
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate()
+{
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 60) * 360) + 180;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
